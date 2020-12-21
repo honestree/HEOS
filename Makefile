09047efa3eba2b0678131f414a2ef94fe2374b56
honestree <kuohh@cs.nctu.edu.tw>
2020-12-21 08:13:12 +0000

    Separate OPENGL part into another function

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 
 CXX = g++
 GL_FLAG = -lglfw -lGL -lm -lX11 -lpthread -lXi -lXrandr -ldl -lGLEW
-ModelDemonstrator_O = MatrixControl.o loadShader.o
+ModelDemonstrator_O = MatrixControl.o loadShader.o ModelDemonstrator.o
 
 LIBARY_O = ${ModelDemonstrator_O}
 
@@ -10,10 +10,13 @@ LIBARY_O = ${ModelDemonstrator_O}
 all: ${LIBARY_O} HEOS
 
 MatrixControl.o: ModelDemonstrator/MatrixControl.hpp ModelDemonstrator/MatrixControl.cpp
-	${CXX} -c ModelDemonstrator/MatrixControl.cpp ${GL_FLAG}
+	${CXX} -c ModelDemonstrator/MatrixControl.cpp
 
 loadShader.o: ModelDemonstrator/loadShader.cpp ModelDemonstrator/loadShader.hpp
-	${CXX} -c ModelDemonstrator/loadShader.cpp ${GL_FLAG}
+	${CXX} -c ModelDemonstrator/loadShader.cpp
+
+ModelDemonstrator.o: ModelDemonstrator/ModelDemonstrator.cpp ModelDemonstrator/ModelDemonstrator.hpp
+	${CXX} -c ModelDemonstrator/ModelDemonstrator.cpp
 
 HEOS: main.cpp ${LIBARY_O}
 	${CXX} main.cpp ${LIBARY_O} -o HEOS ${GL_FLAG}
---
+++ ModelDemonstrator/ModelDemonstrator.cpp
@@ -0,0 +1,237 @@
+//Include std
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//Include GLEW - Always include it before gl.h and glfw3.h, since it's a bit magic.
+#include <GL/glew.h>
+// Include GLFW
+#include <GLFW/glfw3.h>
+// Include GLM
+#include <glm/glm.hpp>
+//transform
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "loadShader.hpp"
+#include "MatrixControl.hpp"
+#include "ModelDemonstrator.hpp"
+
+#define GLFW_DLL
+
+using namespace glm;
+
+int ModelDemonstrator(){
+
+    MatrixControl matrixController;
+
+    // Initialise GLFW
+    glewExperimental = true; // Needed for core profile
+    if( !glfwInit() ){
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        return -1;
+    }
+
+    srand (static_cast <unsigned> (time(0)));
+
+	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
+
+	// Open a window and create its OpenGL context
+	GLFWwindow* window; // (In the accompanying source code, this variable is global for simplicity)
+	window = glfwCreateWindow( 1024, 768, "HEOS", NULL, NULL);
+	if( window == NULL ){
+    	fprintf( stderr, "Failed to open GLFW window.\n" );
+	    glfwTerminate();
+	    return -1;
+	}
+	glfwMakeContextCurrent(window); // Initialize GLEW
+	glewExperimental=true; // Needed in core profile
+	if (glewInit() != GLEW_OK) {
+    	fprintf(stderr, "Failed to initialize GLEW\n");
+	    return -1;
+	}
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // mouse movement
+    glfwPollEvents();
+    glfwSetCursorPos(window, 1024/2, 768/2);
+
+
+    //create a Vertex Array Object
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    glEnable( GL_DEPTH_TEST );
+    glDepthFunc(GL_LESS);
+    glEnable(GL_CULL_FACE);
+
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f,-1.0f,-1.0f, // triangle 1 : begin
+        -1.0f,-1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f, // triangle 1 : end
+        1.0f, 1.0f,-1.0f, // triangle 2 : begin
+        -1.0f,-1.0f,-1.0f,
+        -1.0f, 1.0f,-1.0f, // triangle 2 : end
+        1.0f,-1.0f, 1.0f,
+        -1.0f,-1.0f,-1.0f,
+        1.0f,-1.0f,-1.0f,
+        1.0f, 1.0f,-1.0f,
+        1.0f,-1.0f,-1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f,-1.0f,
+        1.0f,-1.0f, 1.0f,
+        -1.0f,-1.0f, 1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f,-1.0f, 1.0f,
+        1.0f,-1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f,-1.0f,-1.0f,
+        1.0f, 1.0f,-1.0f,
+        1.0f,-1.0f,-1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f,-1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f,-1.0f,
+        -1.0f, 1.0f,-1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f,-1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f,-1.0f, 1.0f
+    };
+    // One color for each vertex. They were generated randomly.
+    static GLfloat g_color_buffer_data[] = {
+        0.583f,  0.771f,  0.014f,
+        0.609f,  0.115f,  0.436f,
+        0.327f,  0.483f,  0.844f,
+        0.822f,  0.569f,  0.201f,
+        0.435f,  0.602f,  0.223f,
+        0.310f,  0.747f,  0.185f,
+        0.597f,  0.770f,  0.761f,
+        0.559f,  0.436f,  0.730f,
+        0.359f,  0.583f,  0.152f,
+        0.483f,  0.596f,  0.789f,
+        0.559f,  0.861f,  0.639f,
+        0.195f,  0.548f,  0.859f,
+        0.014f,  0.184f,  0.576f,
+        0.771f,  0.328f,  0.970f,
+        0.406f,  0.615f,  0.116f,
+        0.676f,  0.977f,  0.133f,
+        0.971f,  0.572f,  0.833f,
+        0.140f,  0.616f,  0.489f,
+        0.997f,  0.513f,  0.064f,
+        0.945f,  0.719f,  0.592f,
+        0.543f,  0.021f,  0.978f,
+        0.279f,  0.317f,  0.505f,
+        0.167f,  0.620f,  0.077f,
+        0.347f,  0.857f,  0.137f,
+        0.055f,  0.953f,  0.042f,
+        0.714f,  0.505f,  0.345f,
+        0.783f,  0.290f,  0.734f,
+        0.722f,  0.645f,  0.174f,
+        0.302f,  0.455f,  0.848f,
+        0.225f,  0.587f,  0.040f,
+        0.517f,  0.713f,  0.338f,
+        0.053f,  0.959f,  0.120f,
+        0.393f,  0.621f,  0.362f,
+        0.673f,  0.211f,  0.457f,
+        0.820f,  0.883f,  0.371f,
+        0.982f,  0.099f,  0.879f
+    };
+
+    // This will identify our vertex buffer
+    GLuint vertexbuffer;
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers(1, &vertexbuffer);
+    // The following commands will talk about our 'vertexbuffer' buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    // Give our vertices to OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    GLuint colorbuffer;
+    glGenBuffers(1, &colorbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
+    GLuint programID = LoadShaders( "ModelDemonstrator/vertexshader", "ModelDemonstrator/fragmentshader" );
+
+	do{
+    	// Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
+    	glClear( GL_COLOR_BUFFER_BIT );
+
+	    // Get a handle for our "MVP" uniform
+    	// Only during the initialisation
+	    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+        matrixController.ComputeMatricesFromInputs( window );
+
+        glm::mat4 ProjectionMatrix = matrixController.GetProjMat();
+        glm::mat4 ViewMatrix = matrixController.GetViewMat();
+        glm::mat4 ModelMatrix = glm::mat4(1.0);
+        glm::mat4 mvp = ProjectionMatrix * ViewMatrix * ModelMatrix;
+
+    	// Send our transformation to the currently bound shader, in the "MVP" uniform
+    	// This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
+    	glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
+
+        // 1st attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+                0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+                3,                  // size
+                GL_FLOAT,           // type
+                GL_FALSE,           // normalized?
+                0,                  // stride
+                (void*)0            // array buffer offset
+                );
+
+
+        // 2nd attribute buffer : colors
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+        glVertexAttribPointer(
+                1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+                3,                                // size
+                GL_FLOAT,                         // type
+                GL_FALSE,                         // normalized?
+                0,                                // stride
+                (void*)0                          // array buffer offset
+                );
+
+        /*
+        for( int i = 0 ; i < 12*3 ; ++i){
+            g_color_buffer_data[i*3] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+            g_color_buffer_data[i*3+1] = g_color_buffer_data[i*3];
+            g_color_buffer_data[i*3+2] = g_color_buffer_data[i*3];
+
+        }
+        */
+        glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
+
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 12*3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+        glDisableVertexAttribArray(0);
+
+	    // Swap buffers
+	    glfwSwapBuffers(window);
+	    glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(programID);
+
+    } // Check if the ESC key was pressed or the window was closed
+    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(window) == 0 );
+};
+
