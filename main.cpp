b6b62ed77a33419e5265ac24110f6b163e3bc63e
honestree <kuohh@cs.nctu.edu.tw>
2020-12-28 02:37:22 -0800

    Update Sample code

--- main.cpp
+++ main.cpp
@@ -12,4 +12,17 @@ int main( int argc , char* argv[] ){
     RequestDealer RD;
     RD.ShowModel( atoi( argv[1] ) , atoi( argv[2] ) , atoi( argv[3] ) );
 
+    //Dicide the Render Range(Angstrom, 10^-10 meters)
+    RD.SetRenderRange( 30.0 );
+    
+    //Dicide to Shuffle how many points inside a cube ranged in ( range, -range )
+    //The more, the result is more detailed and slowed.
+    RD.SetRounds( 100000 );
+
+    //Since The Overall model would strongly depend on how you take probability into account
+    //Type in pair( MAX probability, Min probability) to decide the model coloring
+    //For showing N >= 4, its recommented to go with default;
+    //For Larger N, using smaller probability to show more detailed shape
+    RD.SetColorScheme( std::pair<double,double>( 1e27, 1e25 ) );
+
 }
